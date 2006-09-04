e6dcdf35c8d5c9588645c5ccbe9b11c11608b9e7
Hamilton Verissimo <hammett@gmail.com>
2006-09-04 16:53:44 +0000

    - Removed support for meta elements (as they were passed     via get/form and that doesn't look very safe)
    - More test cases were added
    
    - Major refactoring on Graph design and interfaces. However there wasn't any big change 
      on the DataBinder class itself.
    
    
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/branches/NewDataBinder@2245 73e77b4c-caa6-f847-a29a-24ab75ae54b6

---
+++ Binder/Castle.Components.Binder/IConverter.cs
@@ -0,0 +1,46 @@
+// Copyright 2004-2005 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Components.Binder
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Specialized;
+
+	/// <summary>
+	/// Depicts the contract for implementations able to convert an object
+	/// -- usually a string -- to the specified desired type.
+	/// </summary>
+	public interface IConverter
+	{
+		object Convert(Type desiredType, Type inputType, object input, out bool conversionSucceeded);
+		
+//		object Convert(Type desiredType, object input);
+//
+//		object Convert(Type desiredType, object input, out bool conversionSucceeded);
+//
+//		object Convert(Type desiredType, String paramName, NameValueCollection paramList, IDictionary files);
+//
+//		object Convert(Type desiredType, String paramName, IDictionary paramList, IDictionary files);
+//
+//		object Convert(Type desiredType, String paramName, NameValueCollection paramList, IDictionary files, out bool conversionSucceeded);
+//
+//		object Convert(Type desiredType, String paramName, IDictionary paramList, IDictionary files, out bool conversionSucceeded);
+//
+//		object Convert(Type desiredType, String paramName, object input, out bool conversionSucceeded);
+//
+//		bool CanConvert(Type desiredType, object input, out bool exactMatch);
+
+	}
+}
