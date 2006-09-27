839a0d6395b497eb95b5bf5927228a3ceb3feb8f
Roelof Blom <roelof.blom@gmail.com>
2006-09-27 01:27:34 +0000

    A commit for community appreciation of the new build system.
    - Inspired by NHibernate build (common_project.xml has common tasks for the Castle build process, so project build files stay really simple)
    - Assembly references from one place (JIRA issue CASTLE-4) meaning 1.1 and 2.0 builds do not, except for web site projects, interfere with each other.
    - Build directory is named after target framework and configuration (ie. build/net-1.1/debug, build/net-2.0/release)
    - Restructured SharedLibs to allow automatic copying of assemblies. Can be made dependent on framework.
    - AssemblyInfo.cs is generated by build script -> high level of consistency
    - One build file per assembly
    - Toplevel driver build file per 'subsystem' (components.build, InversionOfControl.build, Tools.build etc.). -> simpler all.build file
    
    New targets and changes to existing targets:
    - 'build' (default) target in all.build does not remove entire build dir anymore -> improves speeds of subsequent builds.
    - new 'rebuild' target behaves like the old 'build' target (removes build dir before building).
    - 'clean-vs' target in all.build removes VS and R# artifacts
    - 'clean' target removes build dir for current framework
    - 'cleanall' removes all build configurations and removes VS and R# artifacts
    
    NB. All tests are running OK. Some are modified to make them work under NAnt and VS.NET (basically they were made independent from the build directory).
    NB2. Facilities are not yet being built, I have to catch some sleep :-)
    
    
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/branches/NewBuildSystem@2396 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- MonoRail/Castle.MonoRail.Framework/EngineContextModule.cs
+++ MonoRail/Castle.MonoRail.Framework/EngineContextModule.cs
@@ -16,9 +16,8 @@ namespace Castle.MonoRail.Framework
 {
 	using System;
 	using System.Web;
-	using System.Reflection;
 
-	using Castle.Core.Logging;
+	using Castle.MonoRail.Framework.Adapters;
 
 	/// <summary>
 	/// Provides the services used and shared by the framework. Also 
@@ -27,82 +26,135 @@ namespace Castle.MonoRail.Framework
 	/// </summary>
 	public class EngineContextModule : IHttpModule 
 	{
-		internal const String RailsContextKey = "rails.context";
-		
-		private static bool initialized;
-		private static EngineContextService engineService;
-		private static object locker = new object();
+		internal static readonly String RailsContextKey = "rails.context";
 
-		private ILogger log;
-
+		private static MonoRailServiceContainer container;
+			
 		/// <summary>
 		/// Configures the framework, starts the services
 		/// and application hooks.
 		/// </summary>
+		/// <param name="context"></param>
 		public void Init(HttpApplication context)
 		{
-			GetLoggerFromContext(context);
-			
-			log.Debug("Initializing Module");
-			// In some weird circunstances 
-			// IIS 6 worker process seems to initialize the module
-			// more than once. We protect ourselves from this locking
-			// the initialization process
-			lock(locker)
+			// Possible race condition here, but not really important
+			if (container == null)
 			{
-				if (!initialized)
-				{
-					engineService = new EngineContextService(this, log);
-					engineService.Initialize();
-					
-					initialized = true;
-				}
-				else
-				{
-					log.Debug("Skipped initialization");
-				}
+				container = new MonoRailServiceContainer();
+
+				container.Start();
 			}
 
-			engineService.InitApplicationHooks(context);
+			SubscribeToApplicationHooks(context);
 		}
-		
+
 		public void Dispose()
 		{
-			log.Debug("Dispose");
-			//engineService.Dispose();
 		}
 		
-		/// <remarks>
-		/// The code here looks somewhat ugly, but it was the best I could get.
-		/// </remarks>
-		private void GetLoggerFromContext(HttpApplication context)
+		/// <summary>
+		/// Registers to <c>HttpApplication</c> events
+		/// </summary>
+		/// <param name="context">The application instance</param>
+		private void SubscribeToApplicationHooks(HttpApplication context)
 		{
-			if (log == null)
-			{
-				log = NullLogger.Instance;
-				
-				BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.Instance;
-				if (context.GetType().GetProperty("Container", bindingFlags) != null)
-				{
-					Object container = context.GetType().GetProperty("Container", bindingFlags).GetValue(context, null);
-					if (container != null)
-					{
-						MethodInfo resolveMethod = container.GetType().GetMethod("Resolve", bindingFlags, null, new Type[] { typeof(Type) }, null);
-						ILoggerFactory loggerFactory = (ILoggerFactory) resolveMethod.Invoke(container, new Object[] { typeof(ILoggerFactory) });
-						log = loggerFactory.Create(GetType());
-					}
-				}
-			}
+			context.BeginRequest += new EventHandler(OnBeginRequest);
+			context.EndRequest += new EventHandler(OnEndRequest);
+			context.AcquireRequestState += new EventHandler(OnAcquireRequestState);
+			context.ReleaseRequestState += new EventHandler(OnReleaseRequestState);
+			context.PreRequestHandlerExecute += new EventHandler(OnPreRequestHandlerExecute);
+			context.PostRequestHandlerExecute += new EventHandler(OnPostRequestHandlerExecute);
+			context.Error += new EventHandler(OnError);		    
 		}
+
+		#region Hooks dispatched to extensions
+
+		private void OnBeginRequest(object sender, EventArgs e)
+		{
+			HttpApplication app = (HttpApplication) sender;
+			HttpContext context = app.Context;
+
+			IRailsEngineContext mrContext = CreateRailsEngineContext(context);
+
+			container.extensionManager.RaiseContextCreated(mrContext);
+		}
+
+		private void OnEndRequest(object sender, EventArgs e)
+		{
+			IRailsEngineContext mrContext = ObtainContextFromApplication(sender);
+
+			container.extensionManager.RaiseContextDisposed(mrContext);
+		}
+
+		private void OnAcquireRequestState(object sender, EventArgs e)
+		{
+			IRailsEngineContext mrContext = ObtainContextFromApplication(sender);
+
+			container.extensionManager.RaiseAcquireRequestState(mrContext);
+		}
+
+		private void OnReleaseRequestState(object sender, EventArgs e)
+		{
+			IRailsEngineContext mrContext = ObtainContextFromApplication(sender);
+
+			container.extensionManager.RaiseReleaseRequestState(mrContext);
+		}
+
+		private void OnPreRequestHandlerExecute(object sender, EventArgs e)
+		{
+			IRailsEngineContext mrContext = ObtainContextFromApplication(sender);
+
+			container.extensionManager.RaisePreProcess(mrContext);
+		}
+
+		private void OnPostRequestHandlerExecute(object sender, EventArgs e)
+		{
+			IRailsEngineContext mrContext = ObtainContextFromApplication(sender);
+
+			container.extensionManager.RaisePostProcess(mrContext);
+		}
+
+		private void OnError(object sender, EventArgs e)
+		{
+			IRailsEngineContext mrContext = ObtainContextFromApplication(sender);
+
+			mrContext.LastException = mrContext.UnderlyingContext.Server.GetLastError();
+
+			container.extensionManager.RaiseUnhandledError(mrContext);
+		}
+
+		private static IRailsEngineContext ObtainContextFromApplication(object sender)
+		{
+			HttpApplication app = (HttpApplication) sender;
+			HttpContext context = app.Context;
+
+			return ObtainRailsEngineContext(context);
+		}
+
+		#endregion
 		
+		private IRailsEngineContext CreateRailsEngineContext(HttpContext context)
+		{
+			IRailsEngineContext mrContext = ObtainRailsEngineContext(context);
+
+			if (mrContext == null)
+			{
+				mrContext = new DefaultRailsEngineContext(container, context);
+
+				context.Items[RailsContextKey] = mrContext;
+			}
+
+			return mrContext;
+		}
+
 		internal static bool Initialized
 		{
-			get { return engineService.Initialized; }
+			get { return container != null; }
 		}
 
 		internal static IRailsEngineContext ObtainRailsEngineContext(HttpContext context)
 		{
-			return engineService.ObtainRailsEngineContext(context);
+			return (IRailsEngineContext) context.Items[RailsContextKey];
 		}
 	}
 }
