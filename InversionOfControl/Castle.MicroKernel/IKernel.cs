6803d52b37bd7565afdb1fc7d969329b77826e91
Hamilton Verissimo <hammett@gmail.com>
2006-10-24 10:44:46 +0000

    - Added setter to ReleasePolicy property - Added documentation
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/branches/1.0.x@2804 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- InversionOfControl/Castle.MicroKernel/IKernel.cs
+++ InversionOfControl/Castle.MicroKernel/IKernel.cs
@@ -331,9 +331,9 @@ namespace Castle.MicroKernel
 		IHandler[] GetAssignableHandlers(Type service);
 
 		/// <summary>
-		/// Returns the implementation for <see cref="IReleasePolicy"/>
+		/// Gets or sets the implementation for <see cref="IReleasePolicy"/>
 		/// </summary>
-		IReleasePolicy ReleasePolicy { get; }
+		IReleasePolicy ReleasePolicy { get; set; }
 
 		/// <summary>
 		/// Returns the implementation for <see cref="IDependencyResolver"/>
