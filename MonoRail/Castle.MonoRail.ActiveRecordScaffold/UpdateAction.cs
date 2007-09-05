1f1c34f03642466e873877230d3b628c9f0bc503
Roelof Blom <roelof.blom@gmail.com>
2007-09-05 09:24:12 +0000

    Merged revisions 2964-2966 via svnmerge from https://svn.castleproject.org/svn/castle/trunk
    ........
      r2964 | hammett | 2006-11-18 04:10:49 +0100 (za, 18 nov 2006) | 3 lines
      
      - Fixed MR-168 
        "ValidateEmail attribute does not work properly in Monorail Scarfold"
    ........
      r2965 | hammett | 2006-11-18 17:41:22 +0100 (za, 18 nov 2006) | 4 lines
      
      - Fixed MR-169
        "Scarfold record modification results exception Can't edit without 
        the proper id when validation is failed in the defined validation rule"
    ........
      r2966 | hammett | 2006-11-18 19:48:45 +0100 (za, 18 nov 2006) | 2 lines
      
      - Fixed MR-165 "DataBind doesn't bind NullableDateTime"
    ........
    
    
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/branches/1.0.x@4246 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- MonoRail/Castle.MonoRail.ActiveRecordScaffold/UpdateAction.cs
+++ MonoRail/Castle.MonoRail.ActiveRecordScaffold/UpdateAction.cs
@@ -15,7 +15,8 @@
 namespace Castle.MonoRail.ActiveRecordScaffold
 {
 	using System;
-
+	using System.Collections;
+	using System.Reflection;
 	using Castle.ActiveRecord;
 	using Castle.Components.Common.TemplateEngine;
 	using Castle.MonoRail.Framework;
@@ -40,13 +41,15 @@ namespace Castle.MonoRail.ActiveRecordScaffold
 
 		protected override void PerformActionProcess(Controller controller)
 		{
+			object instance = null;
+			
 			try
 			{
 				AssertIsPost(controller);
 				
-				object instance = binder.BindObject(Model.Type, Model.Type.Name, builder.BuildSourceNode(controller.Request.Form));
+				instance = binder.BindObject(Model.Type, Model.Type.Name, builder.BuildSourceNode(controller.Request.Form));
 
-				CommonOperationUtils.SaveInstance(instance, controller, errors, prop2Validation, false);
+				CommonOperationUtils.SaveInstance(instance, controller, errors, ref prop2Validation, false);
 
 				SessionScope.Current.Flush();
 				
@@ -66,15 +69,24 @@ namespace Castle.MonoRail.ActiveRecordScaffold
 
 			if (errors.Count != 0)
 			{
+				controller.Context.Flash[Model.Type.Name] = instance;
 				controller.Context.Flash["errors"] = errors;
 
+				PropertyInfo keyProp = ObtainPKProperty();
+				IDictionary props = new Hashtable();
+
+				if (instance != null)
+				{
+					props[keyProp.Name] = keyProp.GetValue(instance, null);
+				}
+				
 				if (UseModelName)
 				{
-					controller.Redirect(controller.AreaName, controller.Name, "edit" + Model.Type.Name, controller.Request.Form);
+					controller.Redirect(controller.AreaName, controller.Name, "edit" + Model.Type.Name, props);
 				}
 				else
 				{
-					controller.Redirect(controller.AreaName, controller.Name, "edit", controller.Request.Form);
+					controller.Redirect(controller.AreaName, controller.Name, "edit", props);
 				}
 			}
 		}
